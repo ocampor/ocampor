99fd3e3813b5d9316ec7dd3ce4f36f74bc4140d5
Ricardo Ocampo <me@ocampor.ai>
2020-01-03 14:24:51 -0600

    Refactor functions to react components

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,16 @@ import { Avatar, Button, Layout, Row, Typography } from 'antd';
 const { Content } = Layout;
 
 //TODO: Move this to a configuration file
-let createSocialLinks = () => {
-  let icons = [
-    { icon: "medium", href: "https://medium.com/@ocampor" },
-    { icon: "github", href: "https://github.com/ocampor" },
-    { icon: "linkedin", href: "https://www.linkedin.com/in/ocampor" },
-    { icon: "twitter", href: "https://twitter.com/ocampo_vr" },
-    { icon: "google", href: "mailto: me@ocampor.ai" }
-  ]
+const iconsMeta = [
+  { icon: "medium", href: "https://medium.com/@ocampor" },
+  { icon: "github", href: "https://github.com/ocampor" },
+  { icon: "linkedin", href: "https://www.linkedin.com/in/ocampor" },
+  { icon: "twitter", href: "https://twitter.com/ocampo_vr" },
+  { icon: "google", href: "mailto: me@ocampor.ai" }
+]
 
-  return icons.map(item => (
+const SocialLinks = ({ meta }) => {
+  return meta.map(item => (
     <div style={{ padding: '0 5px' }}>
       <Button
         shape="circle"
@@ -25,13 +25,13 @@ let createSocialLinks = () => {
   ))
 }
 
-let createProfile = () => {
+const Profile = () => {
   let profileDetails = [
     <Avatar style={{ margin: '2.5em' }} size={128} src="profile.png"/>,
     <Typography.Title style={{ margin: '0' }} level={2}>Ricardo Ocampo</Typography.Title>,
     <Typography.Title level={4} type="secondary">Data Scientist</Typography.Title>,
     <div style={{ display: 'flex', margin: '2.5em' }}>
-      {createSocialLinks()}
+      <SocialLinks meta={iconsMeta}/>
     </div>
   ]
 
@@ -45,7 +45,7 @@ let createProfile = () => {
 export default () => (
   <Content>
     <div style={{ margin: '10% 0%' }}>
-      {createProfile()}
+      <Profile/>
     </div>
   </Content>
 )
